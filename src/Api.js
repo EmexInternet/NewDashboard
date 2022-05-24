88b15aed74838e77c763a19b08a7aa5282a68a9c
Gustavo Assis <assisgustavo@id.uff.br>
2022-05-24 17:41:48 -0300

    Final Day

---
+++ src/Api.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+const api = axios.create({
+
+    baseURL: 'http://200.229.156.14/login',
+    headers: {'Content-Type': 'application/json; charset=utf-8',},
+    auth: {
+        username: process.env.REACT_APP_USERNAME,
+        password: process.env.REACT_APP_PASSWORD,
+        }}
+        );
+
+        export default api;
--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,15 @@ import logo from './logo.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
        <header>Notificação de parada</header>
-    <nav><script src="App.js"></script></nav>
+    <nav>Fila URA</nav>
 
     <main class="Indicadores">
         <item class="Indi">TMA</item>
-        <item class="Indi">TME</item>
+        <item class="Indi">TME
+        </item>
         <item class="Indi">TMO</item>
         <item class="Indi">RECEBIDAS</item>
         <item class="Indi">ATENDIDAS</item>
